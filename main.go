1b7424d2c791cd8ce2c46a27a96e9df96e43e788
Sieep-Coding <nstamb6@gmail.com>
2024-05-08 06:18:12 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Todo struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	IsCompleted bool   `json:"isCompleted"`
+}
+
+var todos = []Todo{
+	{Id: 1, Name: "Learn Go", IsCompleted: false},
+	{Id: 2, Name: "Learn Alpine", IsCompleted: false},
+	{Id: 3, Name: "Go to the gym", IsCompleted: true},
+}
+
+var templates map[string]*template.Template
+
+func init() {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+	}
+
+	templates["index.html"] = template.Must(template.ParseFiles("index.html"))
+}
+
+// handlers
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := templates["index.html"]
+	tmpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
